9aba126a96f6b72b7fc17edd8395dae2d9cc7abc
yakkovwaxelbom <0583281019v@gmail.com>
2026-01-22 12:45:54 +0200

    k8s_done

--- app/main.py
+++ app/main.py
@@ -22,6 +22,5 @@ register_error_handlers(app)
 app.include_router(upload_route)
 
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
-    
+# if __name__ == '__main__':
+#     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
